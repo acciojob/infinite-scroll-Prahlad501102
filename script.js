7069b98865e497447958eca28b14d96458a81338
PrahladKumar <prahladchoudhary7980@gmail.com>
2025-12-19 01:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+
+let count = 0;
+
+// function to add items
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    count++;
+    const li = document.createElement("li");
+    li.innerText = "Item " + count;
+    list.appendChild(li);
+  }
+}
+
+addItems(10);
+
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
